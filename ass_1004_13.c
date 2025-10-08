b260f2cd203e4a25056fde899a9cf48842f265c1
tanvibhadkoliya3018 <tanvibhadkoliya3018@gmail.com>
2025-10-08 10:39:02 +0530

    Add files via upload

---
+++ ass_1004_13.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+float length,breadth,area;
+printf("enter the length of the rectangle:");
+scanf("%f",&length);
+printf("enter the breadth of the rectangle:");
+scanf("%f",&breadth);
+area=length*breadth;
+printf("enter the area of rectangle is:%.2f\n",area);
+scanf("%f",&area);
+return 0;
+}
